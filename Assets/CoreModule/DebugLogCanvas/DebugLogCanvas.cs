23dcf1ffbc15897f7c1dd9ed5ac4dd9a10c47f4e
twr <tawara@atali.jp>
2021-05-27 23:41:55 +0900

    FirstCommit

---
+++ Assets/CoreModule/DebugLogCanvas/DebugLogCanvas.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class DebugLogCanvas: MonoBehaviour
+{
+
+    [SerializeField]
+    private Text m_textUI = null;
+
+    private bool status = false;
+
+
+
+    public void SetON()
+    {
+        if (status == true)
+        {
+            return;
+        }
+
+        status = true;
+        GetComponent<Canvas>().enabled = true;
+        Application.logMessageReceived += OnLogMessage;
+    }
+
+    public void SetOFF()
+    {
+        if (status == false)
+        {
+            return;
+        }
+
+        status = false;
+        GetComponent<Canvas>().enabled = false;
+        Application.logMessageReceived -= OnLogMessage;
+    }
+
+    
+    private void Awake()
+    {
+
+
+        SetON();
+
+
+
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+
+    private void OnLogMessage(string i_logText, string i_stackTrace, LogType i_type)
+    {
+        if (string.IsNullOrEmpty(i_logText))
+        {
+            return;
+        }
+
+
+        switch (i_type)
+        {
+            case LogType.Error:
+            case LogType.Assert:
+            case LogType.Exception:
+                i_logText = string.Format("<color=red>{0}</color>", i_logText);
+                break;
+            case LogType.Warning:
+                i_logText = string.Format("<color=yellow>{0}</color>", i_logText);
+                break;
+            default:
+                break;
+        }
+
+
+        m_textUI.text += i_logText + System.Environment.NewLine;
+    }
+
+
+    private IEnumerator OutputTest()
+    {
+        while (true)
+        {
+            switch (UnityEngine.Random.Range(0, 3))
+            {
+                case 0:
+                    Debug.LogFormat("Time:{0}", System.DateTime.Now);
+                    break;
+                case 1:
+                    Debug.LogWarningFormat("Time:{0}", System.DateTime.Now);
+                    break;
+                case 2:
+                    Debug.LogErrorFormat("Time:{0}", System.DateTime.Now);
+                    break;
+
+                default:
+                    break;
+
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+
+}
---
+++ Assets/NatSuite/Examples/WebCam/WebCam.cs
@@ -0,0 +1,84 @@
+/* 
+*   NatCorder
+*   Copyright (c) 2021 Yusuf Olokoba
+*/
+
+namespace NatSuite.Examples {
+
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Recorders;
+    using Recorders.Clocks;
+    using System;
+
+    public class WebCam : MonoBehaviour {
+
+        [Header(@"UI")]
+        public RawImage rawImage;
+        public AspectRatioFitter aspectFitter;
+
+        private WebCamTexture webCamTexture;
+        private MP4Recorder recorder;
+        private IClock clock;
+        private bool recording;
+        private Color32[] pixelBuffer;
+
+
+        #region --Recording State--
+
+        public void StartRecording () {
+            // Start recording
+            clock = new RealtimeClock();
+            recorder = new MP4Recorder(webCamTexture.width, webCamTexture.height, 30);
+            pixelBuffer = webCamTexture.GetPixels32();
+            recording = true;
+        }
+
+        public async void StopRecording () {
+            // Stop recording
+            recording = false;
+            var realpath = await recorder.FinishWriting();
+            // Playback recording
+
+
+            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(realpath, "GalleryTest", "PrincessVideo", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+
+            Debug.Log("Permission result: " + permission);
+
+            // To avoid memory leaks
+            //Destroy(ss);
+
+
+            //Debug.Log($"Saved recording to: {path}");
+            //Handheld.PlayFullScreenMovie($"file://{path}");
+        }
+        #endregion
+
+
+        #region --Operations--
+
+        IEnumerator Start () {
+            // Request camera permission
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+                yield break;
+            // Start the WebCamTexture
+            webCamTexture = new WebCamTexture(1280, 720, 30);
+            webCamTexture.Play();
+            // Display webcam
+            yield return new WaitUntil(() => webCamTexture.width != 16 && webCamTexture.height != 16); // Workaround for weird bug on macOS
+            rawImage.texture = webCamTexture;
+            aspectFitter.aspectRatio = (float)webCamTexture.width / webCamTexture.height;
+        }
+
+        void Update () {
+            // Record frames from the webcam
+            if (recording && webCamTexture.didUpdateThisFrame) {
+                webCamTexture.GetPixels32(pixelBuffer);
+                recorder.CommitFrame(pixelBuffer, clock.timestamp);
+            }
+        }
+        #endregion
+    }
+}
